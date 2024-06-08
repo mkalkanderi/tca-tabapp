daeacef7685efe745cc56f8275c6011ddafef9e4
mkalkanderi <mohammedalkandari8@gmail.com>
2024-06-08 09:36:22 +0300

    Initial Commit

---
+++ tca-tabbasedapp/tca_tabbasedappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_tabbasedappApp.swift
+//  tca-tabbasedapp
+//
+//  Created by Developer on 08/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct tca_tabbasedappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
